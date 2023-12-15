f2ea9cda2a8024907f64467f7d4c2906f7f596e3
Diogo Ribeiro <me@diogoribeiro.dev>
2023-12-15 00:34:28 +0000

    ...

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from "express";
 import { db } from "../server/db";
-import { userIdSchema } from "../server/db/schema/users";
+import { insertUserParams, userIdSchema } from "../server/db/schema/users";
 
 export const deleteUser: RequestHandler = async (req, res, next) => {
   try {
@@ -24,6 +24,32 @@ export const getUser: RequestHandler = async (req, res, next) => {
     const user = await db.user.findUniqueOrThrow({
       where: {
         id: id,
+      },
+    });
+    return res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser: RequestHandler = async (req, res, next) => {
+  try {
+    const id = userIdSchema.parse(req.params.id);
+    if (req.body.user.id !== id)
+      return res.status(401).json({ message: "Unauthorized!" });
+    console.log(req.body);
+    const params = insertUserParams
+      .omit({ password: true })
+      .parse({ name: req.body.name, email: req.body.email });
+    const user = await db.user.update({
+      where: { id: id },
+      data: {
+        name: params.name,
+        email: params.email,
       },
     });
     return res.status(200).json({
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { deleteUser, getUser } from "../controllers/user";
+import { deleteUser, getUser, updateUser } from "../controllers/user";
 import { authMiddleware } from "../middlewares/auth";
 
 const userRouter = Router();
 
 userRouter.use(authMiddleware);
 userRouter.get("/:id", getUser);
+userRouter.put("/:id", updateUser);
 userRouter.delete("/:id", deleteUser);
 
 export default userRouter;
